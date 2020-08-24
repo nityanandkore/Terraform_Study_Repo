8ee806187d0dd4f09c29674a3207f6faa60ce688
Nityanand Kore <nityanandkore@gmail.com>
2020-08-23 22:58:52 -0400

    auto push

---
+++ GitAutoPush.bat
@@ -0,0 +1,61 @@
+:loop
+
+	
+
+	:: Navigate to the directory you wish to push to GitHub
+
+	::Change <path> as needed. Eg. C:\Users\rich\Desktop\Writings
+
+	cd C:\Users\P2190101\Desktop\NK Personal\NK Study\UNCC\6190 - Cloud Computing\Terraform\Terraform_Study_Repo
+
+	
+
+	::Initialize GitHub
+
+	git init
+
+	
+
+	::Pull any external changes (maybe you deleted a file from your repo?)
+
+	git pull
+
+	
+
+	::Add all files in the directory
+
+	git add --all
+
+	
+
+	::Commit all changes with the message "auto push". 
+
+	::Change as needed.
+
+	git commit -m "auto push"
+
+	
+
+	::Push all changes to GitHub 
+
+	git push
+
+	
+
+	::Alert user to script completion and relaunch.
+
+	echo Complete. Relaunching...
+
+	
+
+	::Wait 300 seconds until going to the start of the loop.
+
+	::Change as needed.
+
+	TIMEOUT 300
+
+	
+
+::Restart from the top.	
+
+goto loop
